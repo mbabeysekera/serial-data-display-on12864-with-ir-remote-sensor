ee51a554012cf5ce29c212da78bb028963fce15b
babeysekera <revolutiongc.lk@gmail.com>
2024-12-08 15:42:26 +0530

    inital commit

---
+++ atmega-32-lcd12864-ir-serial/atmega-32-lcd12864-ir-serial/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 12/8/2024 3:27:16 PM
+ *  Author: Buddhika
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
